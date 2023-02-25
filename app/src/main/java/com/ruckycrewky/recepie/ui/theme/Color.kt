d9c138b3856d15d09ff1a2e691679378c6b55c9e
muratom <muratom73@gmail.com>
2023-02-25 16:57:25 +0300

    init stub

---
+++ app/src/main/java/com/ruckycrewky/recepie/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.ruckycrewky.recepie.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFF616161)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val GrayCookTime = Color(0xFF6F6F6F)
+
+val HighRating = Color(0xFF1D9700)
