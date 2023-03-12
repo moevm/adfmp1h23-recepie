3463977274c363ec1a82d1484a6f37b0236618ba
R0st0k <2002rostok@gmail.com>
2023-03-12 11:23:56 +0300

    Update RecipeCatalog with using RecipeRepositiry

--- app/src/main/java/com/ruckycrewky/recepie/ui/page/RecipeCatalog.kt
+++ app/src/main/java/com/ruckycrewky/recepie/ui/page/RecipeCatalog.kt
@@ -7,26 +7,31 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.ruckycrewky.recepie.R
-import com.ruckycrewky.recepie.data.Recipe
 import com.ruckycrewky.recepie.ui.component.RecipeCard
 import com.ruckycrewky.recepie.ui.component.SearchBar
+import com.ruckycrewky.recepie.ui.viewmodel.RecipeCatalogViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun RecipeCatalog(
-    recipes: List<Recipe>,
     onClickMenu: () -> Unit,
-    navController: NavController
+    navController: NavController,
+    viewModel: RecipeCatalogViewModel = viewModel(),
+    modifier: Modifier = Modifier
 ) {
+    val uiState by viewModel.uiState.collectAsState()
     Column {
         SearchBar(
             placeHolder = stringResource(id = R.string.recipe_search_placeholder),
-            onSearchRequest = {  }, // TODO: временная заглушка
+            onSearchRequest = { viewModel.searchRecipes(it) }, // TODO: временная заглушка
             leadingIcon = {
             Image(
                 painter = painterResource(R.drawable.recipe),
@@ -42,7 +47,7 @@ fun RecipeCatalog(
             verticalArrangement = Arrangement.spacedBy(10.dp),
             horizontalArrangement = Arrangement.spacedBy(10.dp)
         ) {
-            items(recipes) {
+            items(uiState.displayedRecipes) {
                 RecipeCard(
                     recipe = it,
                     modifier = Modifier
